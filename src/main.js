ed911978b55ea65f898b5b02aca37eb9af016d29
kirin-qq <370864006@qq.com>
2020-06-06 23:46:59 +0800

    添加normalize.css

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 开箱即用
+import 'normalize.css/normalize.css'
 Vue.config.productionTip = false
 
 new Vue({
